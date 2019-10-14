afafe9be1b1509830e3dfba8f97d68667e8b1f76
Thomas White <thomas.white.ucf@gmail.com>
2019-10-14 19:38:13 -0400

    scraping to articles collection on heroku db with mongoose models

--- server.js
+++ server.js
@@ -7,31 +7,58 @@
 // if you need a refresher on Cheerio.
 
 // Dependencies
-var express = require('express');
-var mongojs = require('mongojs');
-// Require axios and cheerio. This makes the scraping possible
-var axios = require('axios');
-var cheerio = require('cheerio');
+require('dotenv').config();
+let express = require('express');
+// let HANDLEBARS + REQUIRE>>()
+let bodyParser = require('body-parser');
+// let mongojs = require('mongojs');
+let mongoose = require('mongoose');
+let app = express();
+let PORT = process.env.PORT || 3000;
 
-// Initialize Express
-var app = express();
+// Require all models
+var db = require('./models');
+
+// Require axios and cheerio. This makes the scraping possible
+let axios = require('axios');
+let cheerio = require('cheerio');
+
+// Parse request body as JSON     ** --- they had false for configure body parser for AJAX requests in the Setup Heroku example
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Make public a static folder
+app.use(express.static('/'));
+
+mongoose.Promise = global.Promise;
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+// console.log("MONGODB_URI = ", MONGODB_URI)
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// mongoose.connection.once('open', function(){
+// 	console.log('Conection has been made!');
+//   }).on('error', function(error){
+// 	  console.log('Error is: ', error);
+//   });
 
 // Database configuration
-var databaseUrl = 'drudge';
-var collections = [ 'drudge' ];
+// let databaseUrl = 'drudge';
+// let collections = [ 'drudge' ];
 
 // Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on('error', function(error) {
-	console.log('Database Error:', error);
-});
+// var db = mongojs(databaseUrl, collections);
+// db.on('error', function(error) {
+// 	console.log('Database Error:', error);
+// });
 
 // Main route (simple Hello World Message)
 app.get('/', function(req, res) {
 	res.send('Hello world');
 });
-
-// TODO: make two more routes
 
 // Route 1
 // =======
@@ -59,10 +86,12 @@ app.get('/all', function(req, res) {
 			var link = $(element).attr('href');
 
 			// Save these results in an object that we'll push into the results array we defined earlier
-			results.push({
-				title: title,
-				link: link
-			});
+			if (title && link) {
+				results.push({
+					title: title,
+					link: link
+				});
+			}
 		});
 
 		// Log the results once you've looped through each of the elements found with cheerio
@@ -72,12 +101,7 @@ app.get('/all', function(req, res) {
 
 // Route 2
 // =======
-// When you visit this route, the server will
-// scrape data from the site of your choice, and save it to
-// MongoDB.
-// TIP: Think back to how you pushed website data
-// into an empty array in the last class. How do you
-// push it into a MongoDB collection instead?
+// When you visit this route, the server will scrape data from the site of your choice, and save it to MongoDB.
 app.get('/drudge', function(req, res) {
 	axios.get('https://www.drudgereport.com/').then(function(response) {
 		// Load the HTML into cheerio and save it to a variable
@@ -96,27 +120,34 @@ app.get('/drudge', function(req, res) {
 			// var link = $(element).find("a").attr("href");
 			var link = $(element).attr('href');
 
-			// Save these results in an object that we'll push into the results array we defined earlier
-			results.push({
-				title: title,
-				link: link
-			});
+			if (title && link) {
+				// Save these results in an object that we'll push into the results array we defined earlier
+				results.push({
+					title: title,
+					link: link
+				});
+			}
 		});
 
 		// Log the results once you've looped through each of the elements found with cheerio
 		// console.log(results);
-
-		// LIMITED PULLS TO 10
 		for (var i = 0; i < 10; i++) {
-			db.drudge.insert({ title: results[i].title, link: results[i].link });
+			db.Article
+				.create(results[i])
+				.then(function(dbArticle) {
+					// View the added result in the console
+					console.log(dbArticle);
+				})
+				.catch(function(err) {
+					// If an error occurred, log it
+					console.log(err);
+				});
 		}
 	});
 	res.send('Scrape Complete');
 });
 
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
-
-// Listen on port 3000
-app.listen(3000, function() {
-	console.log('App running on port 3000!');
+// Listen on PORT Variable
+app.listen(PORT, function() {
+	console.log(`App running on PORT: ${PORT}`);
 });
